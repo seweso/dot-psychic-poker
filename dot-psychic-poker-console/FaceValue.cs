d36e3514fd5177697cffb23a3bae015648387a0b
Wouter Schut <wouter.schut@gmail.com>
2018-01-31 21:02:18 +0100

    Add FaceValue enum

---
+++ dot-psychic-poker-console/FaceValue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dot_psychic_poker_console
+{
+    public enum FaceValue
+    {
+        FaceAce, 
+        FaceKing,
+        FaceQueen,
+        FaceJack,
+        FaceTen,
+        Face9,
+        Face8,
+        Face7,
+        Face6,
+        Face5,
+        Face4,
+        Face3,
+        Face2,
+    }
+
+
+    
+}

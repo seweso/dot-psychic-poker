5cfc008d47bdcdc0e36e86df070e19e20fe6fa39
Wouter Schut <wouter.schut@gmail.com>
2018-01-31 15:26:10 +0100

    Add HandRank enum

---
+++ dot-psychic-poker-console/HandRank.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dot_psychic_poker_console
+{
+    public enum HandRank
+    {
+        StraightFlush,
+        FourOfAKind,
+        FullHouse,
+        Flush,
+        Straight,
+        ThreeOfAKind,
+        TwoPair, 
+        OnePair,
+        HighCard,
+    }
+}

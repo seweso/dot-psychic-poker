6a81c0971f1574b18caa3c21d9a16e9f7a9306e4
Wouter Schut <wouter.schut@gmail.com>
2018-01-31 21:13:36 +0100

    Add EnumUtil

---
+++ dot-psychic-poker-console/EnumUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dot_psychic_poker_console
+{
+    public static class EnumUtil
+    {
+        public static IEnumerable<T> GetValues<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+    }
+}

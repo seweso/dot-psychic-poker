eaa6dce69dced6734c78e47df0f63460753b6c53
Wouter Schut <wouter.schut@gmail.com>
2018-01-31 21:16:14 +0100

    Suit + to/from Character

--- dot-psychic-poker-console/Suit.cs
+++ dot-psychic-poker-console/Suit.cs
@@ -1,10 +1,33 @@
-﻿namespace dot_psychic_poker_console
+﻿using System;
+
+namespace dot_psychic_poker_console
 {
     public enum Suit
     {
-        Clubs, 
-        Diamonds, 
-        Hearts, 
+        Clubs,
+        Diamonds,
+        Hearts,
         Spades
+    }
+
+
+    public static class SuitUtil
+    {
+        public static string ToCharacter(this Suit suit)
+        {
+            return suit.ToString().Substring(0, 1);
+        }
+
+        public static Suit GetSuit(string suitCharacter)
+        {
+            foreach (var suit in EnumUtil.GetValues<Suit>())
+            {
+                if (suit.ToCharacter() == suitCharacter)
+                {
+                    return suit;
+                }
+            }
+            throw new ArgumentException("Invalid suit: " + suitCharacter);
+        }
     }
 }
